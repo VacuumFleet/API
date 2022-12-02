d5ee436f9531d1f6a5b9e1f48dd40653ad4d73d2
Hugo Poissonnier <hugo.poissonnier@epitech.eu>
2022-12-02 10:36:36 +0100

    Ajout USER / Token user

---
+++ server/app.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import pytz
+from server.routes.user import router as UserRouter
+from server.routes.auth import router as TokenRouter
+import logging
+FORMAT = "%(levelname)s:%(message)s"
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+
+
+load_dotenv()
+
+timezone_paris = pytz.timezone('Europe/Paris')
+
+app = FastAPI(
+    title='vacuumfleet-api',
+    description="Cette API collecte diverses données des robots et régit les interactions entre app mobile et robot",
+    version='0.0.1',
+    license_info={
+        'name':'The MIT License (MIT)', 
+        'url' : 'https://mit-license.org/'
+    },
+)
+
+app.include_router(UserRouter, tags=["User"], prefix="/user")
+
+app.include_router(TokenRouter, tags=["Token"], prefix="/token")
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
+
+
+
---
+++ server/routes/user.py
@@ -0,0 +1,76 @@
+from datetime import timedelta
+from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+from jose import JWTError, jwt
+from server.routes.auth import create_access_token, get_password_hash
+from decouple import config
+from server.database import (
+    add_user,
+    delete_user,
+    retrieve_user,
+    retrieve_users,
+    update_user,
+)
+from server.models.userModel import (
+    ErrorResponseModel,
+    ResponseModel,
+    UserSchema,
+    UpdateUserModel,
+)
+
+ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+router = APIRouter()
+
+@router.post("/", response_description="User data added into the database")
+async def createUser(user: UserSchema = Body(...)):
+    user = jsonable_encoder(user)
+    new_user = await add_user(user)
+    return ResponseModel(new_user, "User added successfully.")
+
+@router.post("/signup", response_description="User signing up into database")
+async def createUser(user: UserSchema = Body(...)):
+    user.password = get_password_hash(user.password)
+    user = jsonable_encoder(user)
+    new_user = await add_user(user)
+    return ResponseModel(new_user, "User added successfully.")
+
+@router.get("/", response_description="Retrieve all users data")
+async def getUsers():
+    users = await retrieve_users()
+    if(users):
+        return ResponseModel(users, "Users data retrieved successfully.")
+    return ResponseModel(users, "Empty list returned")
+
+@router.get("/{id}", response_description="User data retrieve")
+async def getUser(id):
+    user = await retrieve_user(id)
+    if(user):
+        return ResponseModel(user, "User data retrieved successfully.")
+    return ErrorResponseModel("An error occurd",400, "User doesn't exist.")
+
+@router.put("/{id}", response_description="Update user")
+async def updateUser(id: str, user: UpdateUserModel = Body(...)):
+    user = {k: v for k, v in user.dict().items() if v is not None}
+    updated_user = await update_user(id, user)
+    if updated_user:
+        return ResponseModel(
+            "User with ID: {} name update is successful".format(id),
+            "User name updated successfully",
+        )
+    return ErrorResponseModel(
+        "An error occurred",
+        404,
+        "There was an error updating the user data.",
+    )
+
+@router.delete("/{id}", response_description="User data deleted from the database")
+async def delete_user_data(id: str):
+    deleted_user = await delete_user(id)
+    if deleted_user:
+        return ResponseModel(
+            "User with ID: {} removed".format(id), "User deleted successfully"
+        )
+    return ErrorResponseModel(
+        "An error occurred", 404, "User with id {0} doesn't exist".format(id)
+    )
